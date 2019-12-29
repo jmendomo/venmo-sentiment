b072f4be89bcf56098b75c74404f90f154bd2d83
jmendomo <52796823+jmendomo@users.noreply.github.com>
2019-12-29 02:36:48 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,6 +1,9 @@
+'''
 ============
 == README ==
 ============
 
 
 This program requires Python 3 to run. Additionally, please install the emoji library.
+The runtime for the program should be 30-50 minutes. It outputs graphs of transactions sentiment over time, and the t-test summary of p<0.05
+'''
